bd2ca688a89ac86f1c8d8ad007310f7137a1cb8c
Rev090LT <mamontosaimon900@gmail.com>
2023-10-26 01:23:42 +0500

    Task0

--- Tyuiu.TkachukSS.Sprint3.Task0.V23/Program.cs
+++ Tyuiu.TkachukSS.Sprint3.Task0.V23/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tyuiu.TkachukSS.Sprint3.Task0.V23.Lib;
 
 namespace Tyuiu.TkachukSS.Sprint3.Task0.V23
 {
@@ -10,6 +11,38 @@ namespace Tyuiu.TkachukSS.Sprint3.Task0.V23
     {
         static void Main(string[] args)
         {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Ткачук С. С. | РПСб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Ткачук Семён Сергеевич | РПСб-23-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда     *");
+            Console.WriteLine("* по формуле, при X=1,5                                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 1.5;
+            int startValue = 1, stopValue = 10;
+            Console.WriteLine($"Переменная X: {value}");
+            Console.WriteLine($"Старт шага: {startValue}");
+            Console.WriteLine($"Конец шага: {stopValue}");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write($"Сумма ряда = {dataService.GetSumSeries(value, startValue, stopValue)}");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Tyuiu.TkachukSS.Sprint3.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TkachukSS.Sprint3.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TkachukSS.Sprint3.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd2c1ca0-9679-4006-83be-f63d644a28e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
