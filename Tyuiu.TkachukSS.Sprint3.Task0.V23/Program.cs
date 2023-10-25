670363e2b2d4c9e9ae8e857a8f3d1cccd4343e58
Rev090LT <mamontosaimon900@gmail.com>
2023-10-26 01:01:34 +0500

    Add project files.

---
+++ Tyuiu.TkachukSS.Sprint3.Task0.V23/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.TkachukSS.Sprint3.Task0.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
