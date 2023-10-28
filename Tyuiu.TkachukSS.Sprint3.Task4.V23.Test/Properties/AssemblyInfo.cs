12731541299bbbef7f2ab4d4a5bfef9d63a7586f
Rev090LT <mamontosaimon900@gmail.com>
2023-10-28 13:53:49 +0500

    Task4

---
+++ Tyuiu.TkachukSS.Sprint3.Task4.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TkachukSS.Sprint3.Task4.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TkachukSS.Sprint3.Task4.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("acd6f2b4-07ac-4c7b-84ca-bb9bc8ab51e8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.TkachukSS.Sprint3.Task4.V23/Program.cs
+++ Tyuiu.TkachukSS.Sprint3.Task4.V23/Program.cs
@@ -3,44 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.TkachukSS.Sprint3.Task2.V20.Lib;
-namespace Tyuiu.TkachukSS.Sprint3.Task2.V20
+using Tyuiu.TkachukSS.Sprint3.Task4.V23.Lib;
+
+namespace Tyuiu.TkachukSS.Sprint3.Task4.V23
 {
     class Program
     {
         static void Main(string[] args)
         {
             DataService dataService = new DataService();
-
             Console.Title = "Спринт #3 | Выполнил: Ткачук С. С | РПСб-23-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
             Console.WriteLine("* Тема: Алгоритмы цикличекой структуры                                    *");
-            Console.WriteLine("* Задание #2                                                              *");
-            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
             Console.WriteLine("* Выполнил: Ткачук Семён Сергеевич | РПСб-23-1                            *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет         *");
-            Console.WriteLine("* произведение ряда по формуле, при х=1                                   *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5,                        *");
+            Console.WriteLine("* вычислить значение функции y = (cos(x) / x) + 3 *                       *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            int value = 1, startValue = 1, stopValue = 6;
-            Console.WriteLine($"Переменная X: {value}");
-            Console.WriteLine($"Старт шага: {startValue}");
-            Console.WriteLine($"Конец шага: {stopValue}");
+            int start = -5; int stop = 5;
+            Console.WriteLine($"Старт шага: {start}");
+            Console.WriteLine($"Конец шага: {stop}");
 
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
 
-                Console.WriteLine("***************************************************************************");
-                Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
-                Console.WriteLine("***************************************************************************");
-
-            Console.Write($"Сумма ряда = {dataService.GetMultiplySeries(value, startValue, stopValue)}");
-
-            Console.ReadKey();
+            Console.Write($"Произведение и значение функции = {dataService.Calculate(start, stop)}");
         }
     }
 }
